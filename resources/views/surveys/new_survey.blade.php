f9884908e46c9e08151faef68a1b49580fe5dc1a
skipsuva <skip.suva@gmail.com>
2016-10-03 12:53:48 -0400

    add form labels

--- resources/views/surveys/new_survey.blade.php
+++ resources/views/surveys/new_survey.blade.php
@@ -7,34 +7,70 @@
   {!! Form::open(['url' => 'daily-surveys']) !!}
     <div class="survey-question-container">
       <h3>{{$survey->questionOneContent()}}</h3>
+      {!! Form::label('question_1_response', '1') !!}
       {!! Form::radio('question_1_response', '1') !!}
+
+      {!! Form::label('question_1_response', '2') !!}
       {!! Form::radio('question_1_response', '2') !!}
+
+      {!! Form::label('question_1_response', '3') !!}
       {!! Form::radio('question_1_response', '3') !!}
+
+      {!! Form::label('question_1_response', '4') !!}
       {!! Form::radio('question_1_response', '4') !!}
+
+      {!! Form::label('question_1_response', '5') !!}
       {!! Form::radio('question_1_response', '5') !!}
     </div>
     <div class="survey-question-container">
       <h3>{{$survey->questionTwoContent()}}</h3>
+      {!! Form::label('question_2_response', '1') !!}
       {!! Form::radio('question_2_response', '1') !!}
+
+      {!! Form::label('question_2_response', '2') !!}
       {!! Form::radio('question_2_response', '2') !!}
+
+      {!! Form::label('question_2_response', '3') !!}
       {!! Form::radio('question_2_response', '3') !!}
+
+      {!! Form::label('question_2_response', '4') !!}
       {!! Form::radio('question_2_response', '4') !!}
+
+      {!! Form::label('question_2_response', '5') !!}
       {!! Form::radio('question_2_response', '5') !!}
     </div>
     <div class="survey-question-container">
       <h3>{{$survey->questionThreeContent()}}</h3>
+      {!! Form::label('question_3_response', '1') !!}
       {!! Form::radio('question_3_response', '1') !!}
+
+      {!! Form::label('question_3_response', '2') !!}
       {!! Form::radio('question_3_response', '2') !!}
+
+      {!! Form::label('question_3_response', '3') !!}
       {!! Form::radio('question_3_response', '3') !!}
+
+      {!! Form::label('question_3_response', '4') !!}
       {!! Form::radio('question_3_response', '4') !!}
+
+      {!! Form::label('question_3_response', '5') !!}
       {!! Form::radio('question_3_response', '5') !!}
     </div>
     <div class="survey-question-container">
       <h3>{{$survey->questionFourContent()}}</h3>
+      {!! Form::label('question_4_response', '1') !!}
       {!! Form::radio('question_4_response', '1') !!}
+
+      {!! Form::label('question_4_response', '2') !!}
       {!! Form::radio('question_4_response', '2') !!}
+
+      {!! Form::label('question_4_response', '3') !!}
       {!! Form::radio('question_4_response', '3') !!}
+
+      {!! Form::label('question_4_response', '4') !!}
       {!! Form::radio('question_4_response', '4') !!}
+
+      {!! Form::label('question_4_response', '5') !!}
       {!! Form::radio('question_4_response', '5') !!}
     </div>
 
