299e6eb42888533296c1ef45c8ce96017914468a
skipsuva <skip.suva@gmail.com>
2016-10-03 10:15:51 -0400

    add basic survey methods to return question content

--- app/Survey.php
+++ app/Survey.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    //
+    public function question_one_content()
+    {
+      return "How happy are you feeling today?";
+    }
+
+    public function question_two_content()
+    {
+      return "How well did you sleep last night?";
+    }
+
+    public function question_three_content()
+    {
+      return "How likely are you to see a friend today?";
+    }
+
+    public function question_four_content()
+    {
+      return "How pleased are your with your diet today?";
+    }
 }
--- resources/views/surveys/new_survey.blade.php
+++ resources/views/surveys/new_survey.blade.php
@@ -2,5 +2,19 @@
 
 @section('content')
   <h1>New Survey</h1>
-  
+  <h2>On a scale of 1 to 5...</h2>
+
+  <div class="survey-question-container">
+    <h3>{{$survey->question_one_content()}}</h3>
+  </div>
+  <div class="survey-question-container">
+    <h3>{{$survey->question_two_content()}}</h3>
+  </div>
+  <div class="survey-question-container">
+    <h3>{{$survey->question_three_content()}}</h3>
+  </div>
+  <div class="survey-question-container">
+    <h3>{{$survey->question_four_content()}}</h3>
+  </div>
+
 @endsection
